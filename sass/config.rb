80523b6b61f1dfa0c92b623ce50eb3da3d1ed68b
Christopher Mlynarski <Chris@Christophers-MacBook-Air.local>
2017-06-01 00:07:53 -0500

    further updates

--- sass/config.rb
+++ sass/config.rb
@@ -5,10 +5,11 @@ require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "stylesheets"
-sass_dir = "sass"
-images_dir = "img"
-javascripts_dir = "js"
+sass_dir = "./"
+css_dir = "../css"
+images_dir = "../img"
+javascripts_dir = "../js"
+relative_assets = true
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
