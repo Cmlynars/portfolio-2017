c513d51f424c106513874c42f7a67e9bc058dbdf
Christopher Mlynarski <Chris@Christophers-MacBook-Air.local>
2017-06-17 15:53:57 -0500

    fixed config issue

--- config.rb
+++ config.rb
@@ -5,11 +5,11 @@ require 'susy'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-sass_dir = "./"
-css_dir = "../css"
-images_dir = "../img"
-javascripts_dir = "../js"
+sass_dir = "sass"
+css_dir = "css"
+images_dir = "img"
 relative_assets = true
+# javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
